bb8182ee8434af79fb3b238ee153b9c10b661bee
Even <kiki44552002@gmail.com>
2015-08-30 14:36:28 +0800

    introduce react router

--- src/client.js
+++ src/client.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './containers/app';
-import { todo } from './reducers';
+import Root from './containers/Root';
+import BrowserHistory from 'react-router/lib/BrowserHistory';
 
-const store = createStore(todo, window.__data);
 const rootElement = document.getElementById('root');
 
-console.log(App);
 
 React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
+  <Root history={new BrowserHistory()} />,
   rootElement
 );
