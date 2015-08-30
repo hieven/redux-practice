d4075f1284a6838488fb13f372a434a2f1cdd0bf
Even <kiki44552002@gmail.com>
2015-08-30 18:30:20 +0800

    extract route file

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,12 +3,9 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
 import { todo } from '../reducers';
+import { routes } from '../routes';
 
-// Import views here
-import App from './App';
-import TodoApp from '../components/todo';
-
-const store = createStore(todo, window.__data);
+const store = createStore(todo, window.__INITIAL_DATA__);
 
 export default class Root extends Component {
 
@@ -18,16 +15,11 @@ export default class Root extends Component {
 
   render() {
     const { history } = this.props;
+
     return (
       <div>
         <Provider store={store}>
-          {() =>
-            <Router history={history}>
-              <Route component={App}>
-                <Route path="/" component={TodoApp} />
-              </Route>
-            </Router>
-          }
+          {() => <Router history={history} children={routes} />}
         </Provider>
       </div>
     );
