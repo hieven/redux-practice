7ee2997022e3bdc0a726cf02176dabdb1f220d53
Even <kiki44552002@gmail.com>
2015-08-30 14:36:47 +0800

    change for react router

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,29 +1,16 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addTodo } from '../actions/todoAction';
-import AddTodo from '../components/addTodo';
-import TodoList from '../components/todoList';
-
+import React, { Component, PropTypes } from 'react';
 
 export default class App extends Component {
+
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  };
+
   render() {
+
     return (
       <div>
-        <AddTodo
-          onAddClick={text =>
-            console.log('add todo', text)
-          } />
-        <TodoList
-          todos={[{
-            text: 'Use Redux',
-            completed: true
-          }, {
-            text: 'Learn to connect it to React',
-            completed: false
-          }]}
-          onTodoClick={todo =>
-            console.log('todo clicked', todo)
-          } />
+        {this.props.children}
       </div>
     );
   }
